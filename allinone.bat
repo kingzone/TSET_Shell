02c88601595214967e35b6d5bb1f6eede6f16378
kingzone <875061288@163.com>
2013-03-20 17:33:19 +0800

    Add the time calc, and drop call.

--- allinone.bat
+++ allinone.bat
@@ -1,4 +1,8 @@
 @echo off
+
+set starttime=%time%
+
+call drop.bat
 call 000.bat
 call 001.bat
 call 101.bat
@@ -8,3 +12,6 @@ echo Cleaning...
 del *.out
 del *.b
 del *.sql
+
+set endtime=%time%
+echo starttime: %starttime%; endtime: %endtime%; time elapsed: %endtime%-%starttime%
